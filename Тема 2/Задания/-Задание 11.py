2c11bd238b24ef16258b3b8d420e8de1a297b334
Никита <orekhov_2007@bk.ru>
2024-10-09 18:12:10 +0300

    Решение задач

---
+++ Тема 2/Задания/-Задание 11.py
@@ -0,0 +1,11 @@
+# F1 = (x ∨ ¬y) → (w ≡ z)
+# F2 = (x ∨ ¬y) ≡ (w → z)
+print("x,y,w,z")
+for x in range(2):
+    for y in range(2):
+        for w in range(2):
+            for z in range(2):
+                f1 = (x or not(y)) <= (w ==z)
+                f2 = (x or not(y)) == (w <= z)
+                print(x,y,w,z,int(f1),int(f2))
+#ywxz
---
+++ Тема 2/Задания/-Задание 2.py
@@ -0,0 +1,9 @@
+# ((x ≡ y) → (¬z ∨ w)) ≡ ¬ ((w → x) ∨ (y → z))
+print("x,y,w,z")
+for x in range(2):
+    for y in range(2):
+        for w in range(2):
+            for z in range(2):
+                if ((x==y) <= (not(z) or w )) == (not((w <= x ) or ( y <= z))):
+                    print(x,y,w,z)
+# wzyx
